04f8dad5171895c5c19464e4025f879847f37596
Peyman Naderi <naderidefault@gmail.com>
2022-12-15 18:41:29 +0330

    Edite addres links

--- Assets/JavaScript/MainScript.js
+++ Assets/JavaScript/MainScript.js
@@ -1,7 +1,29 @@
-function $(Q){
+function $(Q) {
     return document.querySelector(Q);
 }
 
-$('.Header__Body--HambergerMenu').addEventListener('click', ()=>{
+$('.Header__Body--HambergerMenu').addEventListener('click', () => {
     $('body').classList.toggle('MenuMobileShow');
+    
+    if ($('.headermenu').hasAttribute('style')) {
+
+        $('.headermenu').removeAttribute('style');
+    } else {
+        $('.headermenu').setAttribute('style', 'height:' + $('.headermenu__content').offsetHeight + 'px;');
+    }
+
+})
+
+$('.bottomHeader__filter').addEventListener('click', () => {
+
+    $('body').classList.toggle('headerFilterShow');
+
+    const height = $('.bottomHeaderFilter__form').offsetHeight;
+    
+    if ($('.bottomHeaderFilter').hasAttribute('style')) {
+
+        $('.bottomHeaderFilter').removeAttribute('style');
+    } else {
+        $('.bottomHeaderFilter').setAttribute('style', 'height:' + height + 'px;');
+    }
 })
